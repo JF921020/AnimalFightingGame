8860298e5533c74e397102ebfd27dba05cedf4ad
Jf1020 <921020j@gmail.com>
2025-02-25 23:26:40 +0800

    add:完善boss的攻擊機制

--- src/Animal/Animal.java
+++ src/Animal/Animal.java
@@ -1,26 +1,17 @@
 package Animal;
+import java.util.Random;
 
 public abstract class Animal {
     protected String name;
     public int HP = 20;
-    public int combo = 0;
+    protected Random random = new Random();
     public int choice = 0;
 
     public Animal(String name) {
         this.name = name;
     }
 
-    public int attack(int combo) {
-        if (combo > 2) {
-            System.out.println("玩家(" + name + ")使用了強力攻擊");
-            System.out.println("Boss受到了強力的攻擊，生命值-4");
-            return 4;
-        } else {
-            System.out.println("玩家(" + name + ")使用了普通攻擊");
-            System.out.println("Boss受到了攻擊，生命值-2");
-            return 2;
-        }
-    }
+    public abstract int attack();
 
     public abstract int useSkill();
 
@@ -33,11 +24,9 @@ public abstract class Animal {
         } else if ((user.choice == 1 && boss.choice == 3) ||
                 (user.choice == 2 && boss.choice == 1) ||
                 (user.choice == 3 && boss.choice == 2)) {
-            user.combo++;
-            boss.HP -= user.attack(user.combo);
+            boss.HP -= user.attack();
         } else {
-            boss.combo++;
-            user.HP -= boss.attack(boss.combo);
+            user.HP -= boss.attack();
         }
 
         System.out.println("玩家 HP: " + user.HP);
--- src/Animal/Cat.java
+++ src/Animal/Cat.java
@@ -5,10 +5,23 @@ public class Cat extends Animal {
         super("貓");
         HP = 80;
     }
+
+    @Override
+    public int attack() {
+        if (random.nextDouble() < 0.3) {
+            System.out.println("玩家(" + name + ")使用了強力攻擊");
+            return useSkill();
+        } else {
+            System.out.println("玩家(" + name + ")使用了普通攻擊");
+            System.out.println("Boss受到了攻擊，生命值-15");
+            return 15;
+        }
+    }
+
     @Override
     public int useSkill() {
-        System.out.println("貓爪連擊! Boss HP -5");
-        return 5;
+        System.out.println("貓爪連擊! Boss HP -25");
+        return 25;
     }
 }
 
